50fe3ca4c0f4b58e32ccf917804e18cf069a1dc2
Alehandro Persaud <andrew041994@gmail.com>
2019-12-23 16:24:00 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -40,6 +40,8 @@ def retrieve_first_element_from_array(array)
 end
 
 def retrieve_last_element_from_array(array)
+  retrieve_last_element_from_array=["wow","dog","buddy","arrays!"]
+  retrieve_last_element_from_array[3]
 
 end
 
