d5c8624de4a09b0ac3a7459b697f1898217ad5e0
Alehandro Persaud <andrew041994@gmail.com>
2019-12-23 16:19:20 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -30,7 +30,7 @@ end
 
 def retrieve_element_from_index(array, index_number)
   retrieve_element_from_index= ["i","am","great"]
-  puts retrieve_element_from_index[2]
+  puts retrieve_element_from_index=[2]
 end
 
 def retrieve_first_element_from_array(array)
